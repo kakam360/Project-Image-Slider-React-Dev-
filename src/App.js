0631972e271778301538372d08fd217d2a4b8e53
kakam360 <114217672+kakam360@users.noreply.github.com>
2024-03-09 22:41:21 +0100

    React Project re-mastered

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import ImageSlider from './ImageSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageSlider />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/ImageSlider.js
@@ -0,0 +1,52 @@
+// ImageSlider.js
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './ImageSlider.css'; // Import your CSS file for styling
+
+const images = [
+  'image1.jpg',
+  'image2.jpg',
+  // Add more images as needed
+];
+
+const ImageSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="slider-container">
+      <div className="column left">
+        <TransitionGroup>
+          <CSSTransition key={currentIndex} timeout={1000} classNames="fade">
+            <img src={images[currentIndex]} alt={`left-${currentIndex}`} />
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+
+      <div className="column right">
+        <TransitionGroup>
+          <CSSTransition
+            key={(currentIndex + 1) % images.length}
+            timeout={1000}
+            classNames="fade"
+          >
+            <img
+              src={images[(currentIndex + 1) % images.length]}
+              alt={`right-${(currentIndex + 1) % images.length}`}
+            />
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
+
