8808b8826953d8d4ec96e6cf8102c17a15d49c5b
kakam360 <114217672+kakam360@users.noreply.github.com>
2024-03-10 09:25:26 +0100

    .

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -6,6 +6,12 @@ import './ImageSlider.css'; // Import your CSS file for styling
 const images = [
   'image1.jpg',
   'image2.jpg',
+  'image3.jpg',
+  'image4.jpg',
+  'image5.jpg',
+  'image6.jpg',
+  'image7.jpg',
+  'image8.jpg',
   // Add more images as needed
 ];
 
